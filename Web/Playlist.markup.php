26cdf1fce3bec0e9ba37ff99924eee9e9b590c63
Bronley Plumb <bplumb@geodecisions.com>
2013-11-16 08:24:12 -0500

    Added fix to playlist page to not use spaces in ids.

--- Web/Playlist.markup.php
+++ Web/Playlist.markup.php
@@ -2,14 +2,14 @@
 <link rel="stylesheet" href="css/jquery.playlist.css"/>
 <script>
     $(function() {
-<?php foreach ($playlists as $playlist) { ?>
-            $("#playlistArea<?php echo $playlist; ?>").playlist({playlistName: "<?php echo $playlist; ?>"});
+<?php foreach ($playlists as $key => $playlist) { ?>
+            $("#playlistArea<?php echo $key; ?>").playlist({playlistName: "<?php echo $playlist; ?>"});
 <?php } ?>
     });
 </script>
-<?php foreach ($playlists as $playlist) { ?>
+<?php foreach ($playlists as $key => $playlist) { ?>
     <div class="playlistArea" style="overflow:hidden;padding-bottom:20px;">
         <h1><?php echo $playlist; ?></h1>
-        <div id="playlistArea<?php echo $playlist; ?>"></div>
+        <div id="playlistArea<?php echo $key; ?>"></div>
     </div>
 <?php } ?>
