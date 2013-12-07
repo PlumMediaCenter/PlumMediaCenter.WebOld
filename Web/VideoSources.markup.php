35abc05affe51da45e1e63fbf2c54a743cc45378
Bronley Plumb <bplumb@geodecisions.com>
2013-12-07 14:53:03 -0500

    Added improved validation functionality to the video sources page.

--- Web/VideoSources.markup.php
+++ Web/VideoSources.markup.php
@@ -3,8 +3,8 @@
 <script type="text/javascript" src="plugins/jquery-validation/additional-methods.min.js"></script>
 
 
-<form id="videoSourcesForm">
-    <a  href="#newSourceModal" class="btn" role="button" onclick="openAdd();">Add New Source</a>
+<form id="videoSourcesForm" method="post">
+    <a id="addNewSourceBtn" class="btn" role="button">Add New Source</a>
     <br/>    <br/>
 
     <table class="table table-hover">
@@ -22,8 +22,8 @@
                 <td><?php echo $src->media_type; ?></td>
                 <td><?php echo $src->security_type; ?></td>
                 <td><a href='<?php echo $src->base_url; ?>'><?php echo $src->base_url; ?></a></td>
-                <td><a href="javascript:openEdit('<?php echo $src->location; ?>','<?php echo $src->base_url; ?>','<?php echo $src->media_type; ?>','<?php echo $src->security_type; ?>');">edit</a></td>
-                <td><button type="button" class="close" onclick="deleteVideoSource('<?php echo $src->location; ?>');" aria-hidden="true">&times;</button></td>
+                <td><a class="editSource" data-location="<?php echo $src->location; ?>" data-base-url="<?php echo $src->base_url; ?>" data-media-type="<?php echo $src->media_type; ?>" data-security-type="<?php echo $src->security_type; ?>">Edit</a></td>
+                <td><button type="button" class="close deleteSource" data-location="<?php echo $src->location; ?>" aria-hidden="true">&times;</button></td>
             </tr>
         <?php } ?>
     </table>
@@ -39,17 +39,18 @@
                     <input type="text" style="width:100%;margin-bottom:0px;" id="location" name="location" placeholder="ex: c:/videos/Movies/"/>
                     <?php //this input is used as reference in the edit action....so we can find the correct row in the db to update. ?>
                     <input type="hidden" name="originalLocation"/>
-                    <!--<div id="locationError" style="color: red;margin-bottom:10px;">*Please Include trailing slash</div>-->
+                    <b>*NOTE: </b>This is a file path that the SERVER can see, not your local computer
                 </div>
             </div>
             <div class="row">
+                <br/>
                 <div class="span2">Security Type: </div>
                 <div class="span4">
                     <input type="radio" id="securityTypePublic" name="securityType" checked="checked" value="<?php echo Enumerations::SecurityType_Public; ?>">
                     <label for="securityTypePublic">No Security</label>
-<!--                    &nbsp;
-                    <input type="radio" id="securityTypePrivate" name="securityType"  value="<?php echo Enumerations::SecurityType_LoginRequired; ?>">
-                    <label for="securityTypePrivate">Login Required</label>-->
+                    <!--                    &nbsp;
+                                        <input type="radio" id="securityTypePrivate" name="securityType"  value="<?php echo Enumerations::SecurityType_LoginRequired; ?>">
+                                        <label for="securityTypePrivate">Login Required</label>-->
 
                 </div>
             </div>
@@ -57,17 +58,17 @@
                 <div class="span2">Base URL: </div>
                 <div class="span4">                  
                     <input type="text" style="width:100%;margin-bottom:0px;" name="baseUrl" placeholder="ex: http://localhost/videos/movies/"/>
-                    <!--<div id="urlError" style="color: red;margin-bottom:10px;">*Please Include trailing slash</div>-->
+                    <b>*NOTE: </b>This is a url that already exists. You must serve the videos over http using your web server.
                 </div>
             </div>
             <div class="row">
                 <div class="span2">Media Type: </div>
                 <div class="span4">
                     <input type="radio" id="mediaTypeMovie" name="mediaType" value="<?php echo Enumerations::MediaType_Movie; ?>">
-                    <label for="mediaTypeMovie">Movie</label>
-                    &nbsp;
+                    <label for="mediaTypeMovie">Directory full of movies</label>
+                    &nbsp;<br/>
                     <input type="radio" id="mediaTypeTvShow" name="mediaType"  value="<?php echo Enumerations::MediaType_TvShow; ?>">
-                    <label for="mediaTypeTvShow">Tv Show</label>
+                    <label for="mediaTypeTvShow">Directory full of Tv Shows (Each in its own tv show folder)</label>
                 </div>
             </div>
 
@@ -76,13 +77,10 @@
         <div class="modal-footer">
             <span style="color:red; float:left;" id="addEditMessage"></span>
             <a href="#" class="btn"  data-dismiss="modal" >Close</a>
-            <input id="addSourceBtn" type="submit" class="btn btn-primary" name="addSource" value="Save New" onclick="return validateAddEdit();">
-            <input id="editSourceBtn" type="submit" class="btn btn-primary" name="editSource" value="Save Edit" onclick="return validateAddEdit();">
+            <input id="addSourceBtn" type="submit" class="btn btn-primary" name="addSource" value="Save New">
+            <input id="editSourceBtn" type="submit" class="btn btn-primary" name="editSource" value="Save Edit">
 
         </div>
-    </div>
-    <div style="display:none;">
-        <input id="deleteSource" type="text" name="deleteSource" value=""/>
     </div>
 </form>
 
