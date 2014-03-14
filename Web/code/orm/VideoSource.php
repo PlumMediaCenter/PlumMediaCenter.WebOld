7c935ec45ac547b7d66fcca2a3fa57ef75a79b61
Bronley Plumb <bplumb@geodecisions.com>
2014-03-13 20:26:02 -0400

    Added validation to the VideoSource orm object to guarentee that the data is written to the database cleanly. Added tests to verify adds updates and deletes of the orm\VideoSource object.

--- Web/code/orm/VideoSource.php
+++ Web/code/orm/VideoSource.php
@@ -11,11 +11,43 @@ namespace orm;
  */
 class VideoSource extends \ActiveRecord\Model {
 
-    static $table_name = "video_source";
-    static $primary_key = "location";
+    public function validate() {
+        //location must exist
+        if (file_exists($this->location) == false) {
+            $this->errors->add('location', "Location '$this->location' does not exist");
+        }
+        //location may only have linux slashes, no windows slashes
+        if (strpos($this->location, '\\') > -1) {
+            $this->errors->add('location', "Location may not contain '\\' characters");
+        }
+        //url must be formatted correctly
+        if (preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $this->baseUrl) != 1) {
+            $this->errors->add('baseUrl', "Base Url is not a valid url '$this->baseUrl'.");
+        }
+        //url must end in a slash
+        if (strlen($this->baseUrl) > 0 && substr($this->baseUrl,-1) !== '/') {
+            $this->errors->add('baseUrl', "Url must end in '/'");
+        }
+
+        //mediaType must be a valid value from the enumeration
+        if (\Enumerations\MediaType::IsValid($this->mediaType) === false) {
+            $this->errors->add('mediaType', "'$this->mediaType' is not a valid media type");
+        }
+
+        //mediaType must be a valid value from the enumeration
+        if (\Enumerations\SecurityType::IsValid($this->securityType) === false) {
+            $this->errors->add('securityType', "'$this->securityType' is not a valid security type");
+        }
+    }
+
+    static $validates_presence_of = array(
+        array('securityType', 'baseUrl', 'mediaType', 'location', 'baseUrl')
+    );
+    static $table_name = 'video_source';
+    static $primary_key = 'location';
     static $alias_attribute = array(
         'securityType' => 'security_type',
-        'baseUrl' => 'base_url',        
+        'baseUrl' => 'base_url',
         'mediaType' => 'media_type',
         'refreshVideos' => 'refresh_videos'
     );
--- Web/test/AllTests.php
+++ Web/test/AllTests.php
@@ -1,10 +1,29 @@
 <?php
+require_once(dirname(__FILE__) . '/../config.php');
+require_once(dirname(__FILE__) . '/../Code/Enumerations.class.php');
 require_once(dirname(__FILE__) . '/../Core/Functions.php');
-
 require_once(dirname(__FILE__) . '/simpletest/autorun.php');
+
+require_once(dirname(__FILE__) . '/../Code/lib/php-activerecord/ActiveRecord.php');
+
+//set up database stuff
+ActiveRecord\Config::initialize(function($cfg) {
+    $cfg->set_model_directory(dirname(__FILE__) . '/../Code/');
+    $cfg->set_connections(array(
+        //'development' => 'mysql://username:password@localhost/database_name',
+        'development' => 'mysql://' .
+        config::$dbUsername . ':' .
+        config::$dbPassword . '@' .
+        config::$dbHost . '/' .
+        config::$dbName));
+});
+
+
 $test = new TestSuite('Video test');
 
-$test->addFile(dirname(__FILE__) . '/Code/TestMovie.php');
+$test->addFile(dirname(__FILE__) . '/Code/TestVideoSource.php');
+//$test->addFile(dirname(__FILE__) . '/Code/TestMovie.php');
+
 //$test->addFile(dirname(__FILE__) . '/Code/NfoReader/TestMovieNfoReader.php');
 //$test->addFile(dirname(__FILE__) . '/Code/NfoReader/TestTvEpisodeNfoReader.php');
 //$test->addFile(dirname(__FILE__) . '/Code/NfoReader/TestTvShowNfoReader.php');
