6d742df3038a6d9e02399f4c0ed0b308332aaf23
Bronley Plumb <bplumb@geodecisions.com>
2014-03-13 20:25:05 -0400

    Added function in enumerations to check if a passed in value is a valid value of the enumeration.

--- Web/code/Enumerations.class.php
+++ Web/code/Enumerations.class.php
@@ -2,7 +2,33 @@
 
 namespace Enumerations;
 
-Class GeneratePosters {
+class Enumeration {
+
+    static $cache = [];
+
+    static function IsValid($value) {
+        //get the name of the subclass calling this
+        $enumType = get_called_class();
+        //if we have not yet calculated the values of this enumeration, calculate them and store them in cache
+        if (isset(\Enumerations\Enumeration::$cache[$enumType]) === false) {
+            $refl = new \ReflectionClass($enumType);
+            $enumValues = $refl->getConstants();
+           \Enumerations\Enumeration::$cache[$enumType] = $enumValues;
+        }
+        
+        //grab the enum values for this enum type
+        $values = \Enumerations\Enumeration::$cache[$enumType];
+        //if the value specified is in the list of enum values for this enum, return true. otherwise, the value is not a valid enum value
+        if (in_array($value, $values) === true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
+Class GeneratePosters extends Enumeration {
 
     const None = "none";
     const Missing = "missing";
@@ -10,7 +36,7 @@ Class GeneratePosters {
 
 }
 
-class MediaType {
+class MediaType extends Enumeration {
 
     const Movie = "Movie";
     const TvShow = "TvShow";
@@ -18,7 +44,7 @@ class MediaType {
 
 }
 
-class MetadataManagerAction {
+class MetadataManagerAction extends Enumeration {
 
     const GeneratePosters = "GeneratePosters";
     const ReloadMetadata = "ReloadMetadata";
@@ -28,14 +54,14 @@ class MetadataManagerAction {
 
 }
 
-class SecurityType {
+class SecurityType extends Enumeration {
 
     const Anonymous = "Anonymous";
     const LoginRequired = "LoginRequired";
 
 }
 
-class PlayType {
+class PlayType extends Enumeration {
 
     const Single = "Single";
     const TvShow = "TvShow";
