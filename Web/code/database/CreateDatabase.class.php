43cbdadf157f231df31e37d1f6f7151537e7cad2
Bronley Plumb <bplumb@gfnet.com>
2014-11-23 10:10:56 -0500

    Added some extra output for the Update file. Removed some of the hardcoded values.

--- Web/code/database/CreateDatabase.class.php
+++ Web/code/database/CreateDatabase.class.php
@@ -18,7 +18,8 @@ class CreateDatabase {
         '0.1.1' => 'db0_1_1',
         '0.1.2' => 'db0_1_2',
         '0.1.3' => 'db0_1_3',
-        '0.1.4' => 'db0_1_4'
+        '0.1.4' => 'db0_1_4',
+        '0.1.5' => 'db0_1_5'
     );
 
     function __construct($rootUsername, $rootPassword, $dbHost) {
