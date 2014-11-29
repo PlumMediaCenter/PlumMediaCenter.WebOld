431b2610fdf05aea67f9c31f3c591db389d06684
Bronley Plumb <bplumb@gfnet.com>
2014-11-29 14:26:18 -0500

    Removed hardcoded server version number from api that was left over from development.

--- api/GetServerVersionNumber.php
+++ api/GetServerVersionNumber.php
@@ -1,6 +1,5 @@
 <?php
-echo "0.2.0";
-return;
+
 require_once(dirname(__FILE__) . '/../code/database/CreateDatabase.class.php');
 $serverVersionNumber = CreateDatabase::CurrentDbVersion();
 echo json_encode($serverVersionNumber);
