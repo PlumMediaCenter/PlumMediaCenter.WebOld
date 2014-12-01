15e5f0072ec64b54613229472999c53e2094a35b
Bronley Plumb <bplumb@gfnet.com>
2014-12-01 17:06:26 -0500

    Formatted the video info page a little nicer. Version bump

--- VideoInfo.markup.php
+++ VideoInfo.markup.php
@@ -14,42 +14,43 @@
 <script type="text/javascript">
     var video = <?php echo $videoJson; ?>;
 </script>
-<div class="row marginless" style="margin-top:5px;">
-    <div class="col-md-3 text-center">
-        <div id="videoInfoMainPosterContainer">
-            <a href="<?php echo $video->getPosterUrl(); ?>" data-lightbox="poster">
-                <img src="<?php echo $video->hdPosterUrl; ?>" id="videoInfoMainPoster" >
+<div class="container">
+    <div class="row " style="margin-top:5px;">
+        <div class="col-md-3 text-center">
+            <div id="videoInfoMainPosterContainer">
+                <a href="<?php echo $video->getPosterUrl(); ?>" data-lightbox="poster">
+                    <img src="<?php echo $video->hdPosterUrl; ?>" id="videoInfoMainPoster" >
+                </a>
+            </div>
+            <a id="videoInfoPlayBtn" href="Play.php?videoId=<?php echo $video->videoId; ?>" class="btn btn-primary">
+                <span class="glyphicon glyphicon-play"></span>&nbsp;Play
             </a>
         </div>
-        <a id="videoInfoPlayBtn" href="Play.php?videoId=<?php echo $video->videoId; ?>" class="btn btn-primary">
-            <span class="glyphicon glyphicon-play"></span>&nbsp;Play
-        </a>
+        <div class="col-md-9">
+            <h1 class="text-center"><?php echo $video->title; ?></h1>
+            Rating: <?php echo $video->mpaa; ?>
+            <br/> <br/><?php echo $video->plot; ?>
+        </div>
     </div>
-    <div class="col-md-9">
-        <h1 class="text-center"><?php echo $video->title; ?></h1>
-        Rating: <?php echo $video->mpaa; ?>
-        <br/> <br/><?php echo $video->plot; ?>
-    </div>
-</div>
-<br/>
-<div class="row marginless">
-    <div class="col-md-5" style="border:0px solid red;">
-        <?php
-        include_once("code/functions.php");
-        printTvShowFileList($video);
-        ?>
-    </div>
-    <div class="col-md-7" style="border:0px solid red;">
-        <div id="episodeInfo" class="shadow">
-            <h1 id="title" style="text-align:center;"></h1>
-            <img align="right" id="episodePoster"/>
-            <p>Season <span id="seasonNumber"></span> Episode <span id="episodeNumber"></span>
-                <br/><b>Rating: </b><span id="mpaa"></span>
-                <br/><b>Release Date:</b> <span id="year"></span>
-            </p>
-            <div id="plot"></div>
+    <br/>
+    <div class="row ">
+        <div class="col-md-5" style="border:0px solid red;">
+            <?php
+            include_once("code/functions.php");
+            printTvShowFileList($video);
+            ?>
+        </div>
+        <div class="col-md-7" style="border:0px solid red;">
+            <div id="episodeInfo" class="shadow">
+                <h1 id="title" style="text-align:center;"></h1>
+                <img align="right" id="episodePoster"/>
+                <p>Season <span id="seasonNumber"></span> Episode <span id="episodeNumber"></span>
+                    <br/><b>Rating: </b><span id="mpaa"></span>
+                    <br/><b>Release Date:</b> <span id="year"></span>
+                </p>
+                <div id="plot"></div>
+            </div>
         </div>
     </div>
 </div>
-
 
--- code/database/CreateDatabase.class.php
+++ code/database/CreateDatabase.class.php
@@ -22,7 +22,8 @@ class CreateDatabase {
         '0.1.5' => 'db0_1_5',
         '0.1.6' => 'db0_1_6',
         '0.1.7' => 'db0_1_7',
-        '0.1.8' => 'db0_1_8'
+        '0.1.8' => 'db0_1_8',
+        '0.1.9' => 'db0_1_9'
     );
 
     function __construct($rootUsername, $rootPassword, $dbHost) {
