20b0a42ad68fa41d3b1e19848e35a769a00609ed
Bronley Plumb <bplumb@gfnet.com>
2015-02-17 22:45:45 -0500

    Updated the gulpfile to actually create a min.js file instead of just a concatenated file. Added sourcemaps to the build process.

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,20 +3,23 @@
     var gulp = require('gulp'),
             uglify = require('gulp-uglify'),
             concat = require('gulp-concat'),
+            sourcemaps = require('gulp-sourcemaps'),
             livereload = require('gulp-livereload');
 
     gulp.task('scripts', function() {
         return gulp.src(['app/app.js', 'app/**/*.js', '!app/app.min.js'])
+                .pipe(sourcemaps.init())
                 .pipe(concat('app.min.js'))
-                //.pipe(uglify())
+                .pipe(uglify())
+                .pipe(sourcemaps.write('./'))
                 .pipe(gulp.dest('app'));
     });
 
     gulp.task('watch', ['default'], function() {
         var server = livereload();
         gulp.watch(['index.html', 'assets/**/*.*', 'app/**/*.*', '!app/app.min.js'], ['default']);
-         
-         
+
+
         gulp.watch(['app/**/*.*']).on('change', function(file) {
             server.changed(file.path);
         });
