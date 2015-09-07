7ebb5602089d79ebe6784403f686b9b70de91cef
Bronley Plumb <bplumb@gfnet.com>
2015-09-07 01:14:09 -0400

    Bumped version numbe.r

--- code/database/CreateDatabase.class.php
+++ code/database/CreateDatabase.class.php
@@ -29,7 +29,8 @@ class CreateDatabase {
         '0.3.0' => 'db0_3_0',
         '0.3.1' => 'db0_3_1',
         '0.3.2' => 'db0_3_2',
-        '0.3.3' => 'db0_3_3'
+        '0.3.3' => 'db0_3_3',
+        '0.3.4' => 'db0_3_4'
     );
 
     function __construct($rootUsername, $rootPassword, $dbHost) {
