2d1ceb7030f86cf817e06345c200f997da8ce37d
Bronley Plumb <bronley@gmail.com>
2017-11-28 12:57:47 -0500

    Version bump

--- code/database/CreateDatabase.class.php
+++ code/database/CreateDatabase.class.php
@@ -52,7 +52,8 @@ class CreateDatabase {
         '0.3.23' => 'db0_3_23',
         '0.3.24' => 'db0_3_24',
         '0.3.25' => 'db0_3_25',
-        '0.3.26' => 'db0_3_26'
+        '0.3.26' => 'db0_3_26',
+        '0.3.27' => 'db0_3_27'
     );
 
     function __construct($rootUsername, $rootPassword, $dbHost) {
