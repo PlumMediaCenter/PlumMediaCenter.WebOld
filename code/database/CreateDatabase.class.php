6d00f1ad31449eb31e46d77831cfa21f41cd3828
Bronley Plumb <bronley@gmail.com>
2019-09-29 15:06:06 -0400

    v0.3.37

--- code/database/CreateDatabase.class.php
+++ code/database/CreateDatabase.class.php
@@ -62,7 +62,8 @@ class CreateDatabase
         '0.3.33',
         '0.3.34',
         '0.3.35',
-        '0.3.36'
+        '0.3.36',
+        '0.3.37'
     ];
 
     function __construct($rootUsername, $rootPassword, $dbHost)
