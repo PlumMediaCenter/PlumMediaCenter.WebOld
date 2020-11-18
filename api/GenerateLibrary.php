a8203361efe77d2ae0d39897f7d1e60ce33e6d29
Bronley Plumb <bronley@gmail.com>
2020-11-17 21:29:51 -0500

    Delete missing videos at beginning of libgen

--- api/GenerateLibrary.php
+++ api/GenerateLibrary.php
@@ -1,18 +1,21 @@
 <?php
-//clear the cache
-header('Content-Type: application/json');
 
 $startSeconds = time();
 //allow up to half hour for this script to run
 set_time_limit(1800);
 $result = (object) [];
 require_once(dirname(__FILE__) . '/../code/Library.class.php');
+require_once(dirname(__FILE__) . '/../code/Video.class.php');
+
+//delete missing videos first
+Video::DeleteMissingVideos();
 
 $l = new Library();
 $result->successLoadingFromDatabase = $l->loadFromFilesystem();
 $result->successWritingToDb = $l->writeToDb();
 $result->success = $result->successLoadingFromDatabase && $result->successWritingToDb;
 
+header('Content-Type: application/json');
 echo json_encode($result);
 
 $endSeconds = time();
